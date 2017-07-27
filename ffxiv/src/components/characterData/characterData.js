b418629434c21324498aee1d759cd48b5d101843
Clyde Daniel <seroth77@gmail.com>
2017-07-27 08:30:36 -0400

    added stat block functionality but don't like it much.  Will look to redesign

--- ffxiv/src/components/characterData/characterData.js
+++ ffxiv/src/components/characterData/characterData.js
@@ -1,5 +1,23 @@
 import { bindable } from 'aurelia-framework';
+import $ from 'jquery';
 
 export class CharacterData {
   @bindable specificCharacterData;
+
+  constructor() {
+    this.attributes = {};
+  }
+
+  showStat(key, val) {
+    this.showHideStats(key);
+    this.attributes = val;
+  }
+
+  showHideStats(stat) {
+    let isHidden = $('.' + stat + '-stat-data').hasClass('hidden');
+    $('.stat-block').addClass('hidden');
+    if (isHidden) {
+      $('.' + stat + '-stat-data').removeClass('hidden');
+    }
+  }
 }
